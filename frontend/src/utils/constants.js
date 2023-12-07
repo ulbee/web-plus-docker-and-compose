208c97d10a3622eb9e97d144bbbb940e46635c5c
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-12-08 01:26:17 +0300

    Замена портов для фронта; замена урла для запросов к бэкенду

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 // export const URL = "http://167.235.140.175:3001";
-export const URL = "http://localhost:4000";
+export const URL = "https://api.kpd-p-kotta.nomoredomainsmonster.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
